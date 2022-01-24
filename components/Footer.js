4d70f0d9f8f0d1047664b3006a64565e6721ad88
Innotexak <akuhinnocent2016@gmail.com>
2022-01-24 08:12:18 +0100

    target issues

--- components/Footer.js
+++ components/Footer.js
@@ -5,10 +5,10 @@ export default function Footer(){
                &copy; {new Date().getFullYear()}. All Rights Reserved
            </div>
            <div className="w-full md:w-1/2 text-center text-xl flex justify-around font-semibold hover:text-blue-200 p-3">
-           <a href="https://facebook.com/innotexak" target="_blank">Facebook</a>
-            <a href="https://twitter.com/innotexak" target="_blank">Twitter</a>
-            <a href="https://linkedin.com/in/innotexak" target="_blank">Linkedin</a>
-            <a href="https://github.com/innotexak" target="_blank">Github</a>
+           <a href="https://facebook.com/innotexak" >Facebook</a>
+            <a href="https://twitter.com/innotexak">Twitter</a>
+            <a href="https://linkedin.com/in/innotexak" >Linkedin</a>
+            <a href="https://github.com/innotexak" >Github</a>
            </div>
         </div>
     )
